45bbe50be632efd7592872c536a5b9e12cea9242
Vishwa <vishwavirat774@gmail.com>
2023-06-19 21:41:16 +0530

    squad server setup

---
+++ squadserver/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mangoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+dotenv.config()
+app = express();
+app.use(cors())
+app.use(express.json())
+
+
+// DEV - process.env.MANGODB_URL
+
+mangoose.connect(process.env.MANGODB_URL).then(() => {
+    console.log("Connected to mangoDb")
+}).catch(() => {
+    console.log("Connection to mangoDb failed")
+})
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Welcome to mangoDb Mr.Vishwa")
+})

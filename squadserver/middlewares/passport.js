c1cf22c3573a9995a25490fd8ed31bca0aec9c7b
Vishwa <vishwavirat774@gmail.com>
2023-06-26 20:25:42 +0530

    mongodb template setup

--- squadserver/middlewares/passport.js
+++ squadserver/middlewares/passport.js
@@ -5,7 +5,7 @@ module.exports = function (passport) {
     var opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
     opts.secretOrKey = process.env.JWT_KEY;
-    // console.log('passport opts', opts)
+    // console.log('passport opts', opts) 
     passport.use(new JwtStrategy(opts, async function (jwt_payload, done) {
         // console.log('passport jwt_payload', jwt_payload)
         if (jwt_payload) {
